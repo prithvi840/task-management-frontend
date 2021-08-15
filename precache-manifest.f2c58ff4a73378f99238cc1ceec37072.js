d2735661a415a4c9176c2d81f3bfac7a3a3b332b
prithvi840 <prithvi840@users.noreply.github.com>
2021-08-15 17:07:44 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.f2c58ff4a73378f99238cc1ceec37072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f1647b5d4b7466e8b9ca9a8c0b489d",
+    "url": "https://prithvi840.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "6491238dc1adaf6b8067",
+    "url": "https://prithvi840.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "252365dd6ede62c0e9e5",
+    "url": "https://prithvi840.github.io/task-management-frontend/static/js/2.c48eab1e.chunk.js"
+  },
+  {
+    "revision": "6491238dc1adaf6b8067",
+    "url": "https://prithvi840.github.io/task-management-frontend/static/js/main.c6fc2133.chunk.js"
+  },
+  {
+    "revision": "57742beed5f875874cb4",
+    "url": "https://prithvi840.github.io/task-management-frontend/static/js/runtime~main.b44df435.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://prithvi840.github.io/task-management-frontend/precache-manifest.f2c58ff4a73378f99238cc1ceec37072.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://prithvi840.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
